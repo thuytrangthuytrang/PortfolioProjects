53b64088ce0e147a18235586a68a55b2e4efd989
thuytrangthuytrang <150219703+thuytrangthuytrang@users.noreply.github.com>
2023-12-10 23:54:11 +0700

    Update COVID Portfolio - Data Exploration.sql

--- COVID Portfolio - Data Exploration.sql
+++ COVID Portfolio - Data Exploration.sql
@@ -52,3 +52,81 @@ Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 
 from CovidDeaths
 where location = 'Vietnam'
 order by 1,2
+
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
+Select Location, date, Population, total_cases,  
+(total_cases/population)*100 as PercentPopulationInfected
+From CovidDeaths
+Where location like 'Vietnam'
+order by 1,2
+
+
+-- Countries with Highest Infection Rate compared to Population
+Select Location, Population, MAX(total_cases) as HighestInfectionCount, 
+Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+where total_cases is not null
+Group by Location, Population
+order by MAX(total_cases) desc
+
+
+-- Countries with Highest Death Count per Population--
+
+Select Location, MAX(Total_deaths) as TotalDeathCount
+From CovidDeaths
+Where continent is not null and Total_deaths is not null 
+Group by Location
+order by TotalDeathCount desc
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From CovidDeaths
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+	
+-- GLOBAL NUMBERS
+
+Select SUM(new_cases) as total_cases, 
+	SUM(cast(new_deaths as int)) as total_deaths, 
+	SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths
+where continent is not null 
+Group By date
+order by 1,2
+
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(cast(vac.new_vaccinations as int )) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From CovidDeaths dea
+Join CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null and vac.new_vaccinations is not null
+order by 2,3
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(cast(vac.new_vaccinations as int )) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From CovidDeaths dea
+Join CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null and vac.new_vaccinations is not null
+order by 2,3
+
